9bc7093f256ae04cd17c43881e93351a2445b47c
Sam H <samuelh@missionreadyhq.com>
2024-11-13 18:35:08 +1300

    Update code

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const cityInput = document.getElementById("input");
 const dispalySection = document.getElementById("display");
 
-
-const API_KEY = "650ad8fb3c6e4ecab3190253232706";
+const API_KEY = "e71306dec03d4d56abb211306242509";
 
 const handleClick = () => {
   let currentCity = cityInput.value;
@@ -14,8 +13,7 @@ const handleClick = () => {
   )
     .then((res) => res.json())
     .then((data) => {
-        dispalySection.innerHTML =
-      ` <p>${data.location.name}'s temperature is ${data.current.temp_c}℃.</p>
+      dispalySection.innerHTML = ` <p>${data.location.name}'s temperature is ${data.current.temp_c}℃.</p>
         <p>It's ${data.current.condition.text}.</p>
         <img src="${data.current.condition.icon}" alt="icon"/>
       `;
